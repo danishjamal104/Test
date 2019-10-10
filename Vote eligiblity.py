b78d1b015b4b5c2bd9a91d4e31857a51165fe0ca
RanitPradhan <56351938+RanitPradhan@users.noreply.github.com>
2019-10-10 23:10:15 +0530

    Create Vote eligiblity.py

---
+++ Vote eligiblity.py
@@ -0,0 +1,9 @@
+#Eligiblity For  Votting:-
+n=str(input("Enter your name: "))
+a=int(input("Enter your age: "))
+v=int(input("Enter the eligiblity age vor votting: "))
+if a==v:
+    print("",n,"you are eligible for votting")
+else:
+    print("",n,"you are not eligible for votting")
+    
